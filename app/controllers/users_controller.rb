1c143cb3ee4cbd9c609980967e11caf86f95f52c
pahoffart25 <pahoffart25@gmail.com>
2020-03-02 17:48:30 -0600

    connections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+   @comment = Comment.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get 'login', to: 'sessions#new'
+  # post 'login', to: 'sessions#create'
+  # root :to => 'sessions#welcome'
+  
   resources :users 
   resources :posts
   resources :categories, only: [:index,:show,:new,:create]
